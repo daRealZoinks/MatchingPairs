8876dd2dbcb7b2446f79ccf40bdba21c5f73b011
Andreea Musca <andreeamusca382@gmail.com>
2023-11-29 12:13:52 +0200

    Create user model

---
+++ Game/Models/User.cs
@@ -0,0 +1,33 @@
+﻿using System.Xml.Serialization;
+
+namespace Game.Models
+{
+	public class User
+	{
+		public User()
+		{
+		}
+
+		public User(string username, int totalGames, int gamesWon, string profilePic)
+		{
+			Username = username;
+			TotalGames = totalGames;
+			GamesWon = gamesWon;
+			ProfilePic = profilePic;
+		}
+
+		[XmlElement("Username")]
+		public string Username { get; set; } 
+
+		[XmlElement("TotalGames")]
+		public int TotalGames { get; set; } 
+
+		[XmlElement("GamesWon")]
+		public int GamesWon { get; set; }
+
+		[XmlElement("ProfilePic")]
+		public string ProfilePic { get; set; }
+
+    }
+
+}
