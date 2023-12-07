6a863dba0b5d16c1ed30fe2768bef9bd6eec6bc8
Andreea Musca <andreeamusca382@gmail.com>
2023-12-07 14:38:09 +0200

    Create SelectGame window

--- Game/ViewModels/MainContentViewModel.cs
+++ Game/ViewModels/MainContentViewModel.cs
@@ -7,15 +7,22 @@ namespace Game.ViewModels;
 public class MainContentViewModel
 {
 	public ICommand SwitchToUserControl2Command { get; private set; }
+	public ICommand PlayCommand { get; private set; }
 
 	public MainContentViewModel()
 	{
 		SwitchToUserControl2Command = new RelayCommand(SwitchToUserControl2);
+		PlayCommand = new RelayCommand(Play);
 	}
 
 	private void SwitchToUserControl2()
 	{
 		NavigationService.GetInstance().NavigateToPage<ChangeUserView>();
+	}
+
+	private void Play()
+	{
+		NavigationService.GetInstance().NavigateToPage<SelectGameView>();
 	}
 }
 
---
+++ Game/Views/SelectGameView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Game.ViewModels;
+using System.Windows.Controls;
+
+namespace Game.Views;
+
+/// <summary>
+/// Interaction logic for SelectGameView.xaml
+/// </summary>
+public partial class SelectGameView : UserControl
+{
+	public SelectGameView()
+	{
+		InitializeComponent();
+		DataContext = new SelectGameViewModel();
+	}
+}
