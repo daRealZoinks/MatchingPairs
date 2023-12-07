55b7acf2d446482d199bf18b56a7045a2e58f0b1
Andreea Musca <andreeamusca382@gmail.com>
2023-12-07 17:09:54 +0200

    Create leaderboard screen

--- Game/ViewModels/MainContentViewModel.cs
+++ Game/ViewModels/MainContentViewModel.cs
@@ -8,11 +8,12 @@ public class MainContentViewModel
 {
 	public ICommand SwitchToUserControl2Command { get; private set; }
 	public ICommand PlayCommand { get; private set; }
-
+	
 	public MainContentViewModel()
 	{
 		SwitchToUserControl2Command = new RelayCommand(SwitchToUserControl2);
 		PlayCommand = new RelayCommand(Play);
+		
 	}
 
 	private void SwitchToUserControl2()
@@ -24,5 +25,6 @@ public class MainContentViewModel
 	{
 		NavigationService.GetInstance().NavigateToPage<SelectGameView>();
 	}
+
 }
 
---
+++ Game/Views/LeaderboardView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Game.ViewModels;
+using System.Windows.Controls;
+
+namespace Game.Views;
+
+/// <summary>
+/// Interaction logic for LeaderboardView.xaml
+/// </summary>
+public partial class LeaderboardView : UserControl
+{
+	public LeaderboardView()
+	{
+		InitializeComponent();
+		DataContext = new LeaderboardViewModel();
+	}
+}
