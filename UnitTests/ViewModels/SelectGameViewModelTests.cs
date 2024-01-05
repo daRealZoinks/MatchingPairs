c14c7d1bcb460cfd86eecdd206addf5107bb3234
Zoinks <murarashuvlad@gmail.com>
2024-01-05 11:56:33 +0200

    added test framework for select game view model

---
+++ UnitTests/ViewModels/SelectGameViewModelTests.cs
@@ -0,0 +1,30 @@
+﻿namespace UnitTests.ViewModels
+{
+    [TestClass]
+    public class SelectGameViewModelTests
+    {
+        [TestMethod]
+        public void SwitchToLeaderboardTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void GoBackTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void NewGameTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void OpenGameTest()
+        {
+
+        }
+    }
+}
